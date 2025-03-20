8ba9c3fdaf622d102abb1d3b8ddfbd33b48806b3
Chaan0210 <chan060304@gmail.com>
2025-03-20 20:32:19 +0900

    style: font, markdown 적용

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
 import sqlite3
+import markdown
 
 app = Flask(__name__)
+
+def convert_markdown(text):
+    return markdown.markdown(text)
 
 def get_db_connection():
     conn = sqlite3.connect("papers.db")
@@ -11,8 +15,15 @@ def get_db_connection():
 @app.route("/")
 def index():
     conn = get_db_connection()
-    papers = conn.execute("SELECT * FROM papers").fetchall()
+    rows = conn.execute("SELECT * FROM papers ORDER BY CAST(num_vote AS INTEGER) DESC").fetchall()
     conn.close()
+
+    papers = [dict(row) for row in rows]
+
+    for paper in papers:
+        paper["summary_md"] = convert_markdown(paper["summary"])
+        paper["translated_abstract_md"] = convert_markdown(paper["translated_abstract"])
+
     return render_template("index.html", papers=papers)
 
 if __name__ == "__main__":
